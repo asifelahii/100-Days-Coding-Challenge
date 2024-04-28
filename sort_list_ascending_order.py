bf65467f498381481f6150a97291702d0f82cb03
ASIF ELAHI <149480370+asifelahii@users.noreply.github.com>
2024-04-29 02:28:23 +0600

    A program to sort a list in ascending order
    Title: Sort List in Ascending Order
    
    Problem Statement:
    ------------------
    Write a program to sort a list of grades in ascending order.
    
    Solution Explanation:
    ---------------------
    1. Create an empty list to store students' grades.
    2. Prompt the user to enter five grades and append them to the list.
    3. Print the entered list to display the unsorted grades.
    4. Use the `sort()` method to sort the grades in ascending order.
    5. Print the sorted list to display the grades in ascending order.
    
    Source Code Explanation with Comments:
    --------------------------------------
    1. Create an empty list named 'list' to store students' grades.
    2. Prompt the user to input five grades sequentially and append them to the list.
    3. Print the entered list to display the unsorted grades.
    4. Sort the grades list using the `sort()` method to arrange grades in ascending order.
    5. Print the sorted list to display the grades in ascending order.
    
    Steps:
    ------
    1. Create an empty list to store students' grades.
    2. Prompt the user to input five grades sequentially and append them to the list.
    3. Print the entered list to display the unsorted grades.
    4. Sort the grades list to arrange grades in ascending order.
    5. Print the sorted list to display the grades in ascending order.

---
+++ sort_list_ascending_order.py
@@ -0,0 +1,13 @@
+"""Write a programme to sort a list in ascending order."""
+list = [] # creating an empty list
+
+# Taking input from user & appending to list.
+list.append(input("Enter students grade: "))
+list.append(input("Enter students grade: "))
+list.append(input("Enter students grade: "))
+list.append(input("Enter students grade: "))
+list.append(input("Enter students grade: "))
+
+print(list) # prints original list.
+list.sort() # sorts the list.
+print(list) # prints the sorted list.
