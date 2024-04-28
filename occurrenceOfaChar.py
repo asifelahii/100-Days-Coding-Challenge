d27d8e0602acf77d346753439cf5d92adbfa292b
ASIF ELAHI <149480370+asifelahii@users.noreply.github.com>
2024-04-29 02:02:03 +0600

    correction of incorrect spelling
    Before:
    incorrect = "tring"
    After: 
    correct: "string"

--- occurrenceOfaChar.py
+++ occurrenceOfaChar.py
@@ -1,3 +1,3 @@
-"""Write a programme to occurrence (count) '$' in a tring."""
+"""Write a programme to occurrence (count) '$' in a string."""
 str = input("Enter a string containing '$': ")
 print(str.count('$'))
